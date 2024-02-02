bcebebb268846a184c4d87b730dec001d7c38d55
kvdmitrieva <katedv02@gmail.com>
2024-02-02 22:49:27 +0300

    add wandb & simple hydra

--- management_and_testing/main.py
+++ management_and_testing/main.py
@@ -3,18 +3,28 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
 
+import argparse
+import hydra
+import omegaconf
+import wandb
+
 from modeling.diffusion import DiffusionModel
 from modeling.training import generate_samples, train_epoch
 from modeling.unet import UnetModel
 
 
-def main(device: str, num_epochs: int = 100):
+@hydra.main(config_name="config")
+def main(cfg, wandb_key: str, device: str, num_epochs: int = 100):
+    wandb.login(key=wandb_key)
+    wandb.init(project=cfg.wandb.project, name=cfg.wandb.name)
+    wandb.config = omegaconf.OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+
     ddpm = DiffusionModel(
-        eps_model=UnetModel(3, 3, hidden_size=128),
-        betas=(1e-4, 0.02),
-        num_timesteps=1000,
+        eps_model=UnetModel(cfg.model.unet.in_channels, cfg.model.unet.out_channels, hidden_size=cfg.model.unet.hidden_size),
+        betas=(cfg.model.beta1, cfg.model.beta2),
+        num_timesteps=cfg.model.num_timestamps,
     )
-    ddpm.to(device)
+    ddpm = ddpm.to(device)
 
     train_transforms = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
@@ -27,8 +37,8 @@ def main(device: str, num_epochs: int = 100):
         transform=train_transforms,
     )
 
-    dataloader = DataLoader(dataset, batch_size=128, num_workers=4, shuffle=True)
-    optim = torch.optim.Adam(ddpm.parameters(), lr=1e-5)
+    dataloader = DataLoader(dataset, batch_size=cfg.training.batch_size, num_workers=cfg.training.num_workers, shuffle=True)
+    optim = torch.optim.Adam(ddpm.parameters(), lr=cfg.training.lr)
 
     for i in range(num_epochs):
         train_epoch(ddpm, dataloader, optim, device)
@@ -36,5 +46,14 @@ def main(device: str, num_epochs: int = 100):
 
 
 if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument(
+        "-k",
+        "--key",
+        default=None,
+        type=str,
+        help="wandb key for logging",
+    )
+    args = args.parse_args()
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    main(device=device)
+    main(wandb_key=args.key, device=device)
--- management_and_testing/modeling/training.py
+++ management_and_testing/modeling/training.py
@@ -2,7 +2,11 @@ import torch
 from torch.optim.optimizer import Optimizer
 from torch.utils.data import DataLoader
 from torchvision.utils import make_grid, save_image
+from torchvision.transforms.functional import to_pil_image
+
 from tqdm import tqdm
+
+import wandb
 
 from modeling.diffusion import DiffusionModel
 
@@ -16,14 +20,33 @@ def train_step(model: DiffusionModel, inputs: torch.Tensor, optimizer: Optimizer
     return loss
 
 
-def train_epoch(model: DiffusionModel, dataloader: DataLoader, optimizer: Optimizer, device: str):
+def train_epoch(model: DiffusionModel, dataloader: DataLoader, optimizer: Optimizer,
+                device: str, log_metrics: bool = False, log_media: bool = False):
     model.train()
     pbar = tqdm(dataloader)
     loss_ema = None
     for x, _ in pbar:
         train_loss = train_step(model, x, optimizer, device)
         loss_ema = train_loss if loss_ema is None else 0.9 * loss_ema + 0.1 * train_loss
+        if log_metrics:
+            wandb.log(
+                {
+                    'train_loss': loss_ema,
+                    'learning_rate': optimizer.defaults['lr']
+                }
+            )
         pbar.set_description(f"loss: {loss_ema:.4f}")
+
+    if log_media:
+        input_image = to_pil_image(make_grid(x, nrow=4))
+        generated_image = model(x.to(device)).detach().cpu()
+        generated_image = to_pil_image(make_grid(generated_image, nrow=4))
+        wandb.log(
+            {
+                "Input": wandb.Image(input_image),
+                "Generated images": wandb.Image(generated_image),
+            }
+        )
 
 
 def generate_samples(model: DiffusionModel, device: str, path: str):
